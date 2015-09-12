629815e0610b07e39caefa79f3ef0efd60a4fab0
Sam-Akopyan <sam.ako@yandex.ru>
2015-09-12 14:08:32 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radioportal::Application.config.secret_key_base = '2e0fa9f58f29b65a830fedbffb0b0722e58753208a4530d09b6a14788211abd52314948d7ccf5a8e62d311c7820229ec4584cbbdd6af529d700d858e81b30dbf'
